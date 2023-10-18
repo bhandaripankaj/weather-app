302d8e3f4d8b805a2c6cec5013e5933f0494f37c
pankaj <pankajbhandari933@gmail.com>
2023-10-18 15:27:20 +0530

    worked on weater api

--- src/weather.jsx
+++ src/weather.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import moment from 'moment';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch,faPaperPlane,faCloud ,faTint,faCircle,faUmbrella,faWind,faCloudShowersHeavy} from '@fortawesome/free-solid-svg-icons';
+import { faSearch,faPaperPlane,faTint,faCircle,faUmbrella,faWind} from '@fortawesome/free-solid-svg-icons';
 
 const Weather = () => {
   const [data,setData] = useState({})
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState('chandigarh');
 const [hourly,setHourly] = useState([])
 
   const formatDate = (dateString) => {
@@ -14,24 +14,39 @@ const [hourly,setHourly] = useState([])
     return formattedDate;
   };
 let REACT_APP_API_KEY = process.env.REACT_APP_API_KEY || "10151bf4adff49b48b493946231610"
+
+const APICALL = async()=>{
+  return  axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${REACT_APP_API_KEY}&q=${city}&days=7&aqi=yes&alerts=yes`); 
+}
+
+const getWeather = async() => {
+  try {
+    const response = await APICALL()
+    setData(response.data)
+    setCity(response.data?.location?.name)
+    setHourly(response.data.forecast.forecastday[0].hour)
+} catch (error) {
+  console.error('Error fetching weather data:', error);
+  alert("Please enter another location",)
+}
+};
+
     useEffect(() => {
-      console.log("process.env.API_KEY",process.env.REACT_APP_API_KEY)
       const fetchData = async () => {
         try {
-          const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=${REACT_APP_API_KEY}&q=chandigarh&days=7&aqi=yes&alerts=yes`);
-              setData(response.data)
-              setCity(response.data?.location?.name)
-              setHourly(response.data.forecast.forecastday[0].hour)
+            const response = await APICALL()
+            setData(response.data)
+            setCity(response.data?.location?.name)
+            setHourly(response.data.forecast.forecastday[0].hour)
         } catch (error) {
           console.error('Error fetching weather data:', error);
+         alert("Data not available on your location!",)
         }
       };
-      fetchData();
-    }, data); 
+      fetchData()
+    }, []); 
 
-  console.log("hourly",data)
 
-  let days = ["TUE","WED","THU","FRI","SAT","SUN"]
   return (
     <div className='weather'>
       <div className="search-box">
@@ -41,10 +56,14 @@ let REACT_APP_API_KEY = process.env.REACT_APP_API_KEY || "10151bf4adff49b48b4939
           placeholder="Enter Location"
           value={city}
           onChange={(e) => setCity(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              getWeather();
+            }
+          }}
         />
-              <button onClick={"getWeather"}>
+        <button onClick={getWeather}>
         <FontAwesomeIcon icon={faPaperPlane} /> </button>
-
       </div>
        <p className='location-name'> {data?.location?.country} , {data?.location?.name}</p>
        <p>{formatDate(data?.location?.localtime)}</p>
@@ -52,12 +71,10 @@ let REACT_APP_API_KEY = process.env.REACT_APP_API_KEY || "10151bf4adff49b48b4939
        <p className='day'>Today</p>
         <div className='main'>
           <div className='forecast'>
-         <FontAwesomeIcon icon={faCircle} style={{color:"#ffdc79", position:"absolute",fontSize:'33px',marginLeft:"30px",marginTop:"-8px",border:"2px solid #ccc",borderRadius:"20px" }}/>
-         <FontAwesomeIcon icon={faCloud} style={{ color:"#fafafa", fontSize: '40px',position:"absolute", marginLeft:"-5px",marginTop:"-5px" }} />
-         <FontAwesomeIcon icon={faCloud} style={{ color:"#fafafa", fontSize: '50px',position:"absolute", marginLeft:"38px" }} />
+              <img src= {"https:"+data?.current?.condition?.icon} alt="Weather Icon" />
           <FontAwesomeIcon icon={faCircle} className='circle' />
             <p className='temperature'>{data?.current?.temp_c}°C <br></br>
-            <span>Clouds</span>
+            <span>{data?.current?.condition?.text}</span>
             </p>
           </div>
           <div className='forecast-icon'>
@@ -86,7 +103,8 @@ let REACT_APP_API_KEY = process.env.REACT_APP_API_KEY || "10151bf4adff49b48b4939
           return (
             <div className='hourly-column' key={index}>
               <p>{moment(el.time, "YYYY-MM-DD HH:mm").format("h A")} </p>
-              <FontAwesomeIcon icon={faCloud} style={{ color: "#ffcc02" }} />
+              <img src= {"https:"+el.condition.icon} alt="Weather Icon" />
+
               <p>{el.temp_c}°C</p>
             </div>
           );
@@ -95,15 +113,15 @@ let REACT_APP_API_KEY = process.env.REACT_APP_API_KEY || "10151bf4adff49b48b4939
       })}
     </div>
     <div className='weekly-container'>
-      {data?.forecast?.forecastday.map((el)=>{
+      {data?.forecast?.forecastday.map((el,index)=>{
         if(
           new Date(el.date) > new Date()
         ){
           return (
-            <div className='weekly-column'>
+            <div className='weekly-column' key={index}>
             <p>{moment(el.date, 'YYYY-MM-DD').format('ddd')}</p>
             <p>{el.day.mintemp_c} | {el.day.maxtemp_c}°C</p>
-            <FontAwesomeIcon icon={faCloudShowersHeavy}  />
+            <img src= {"https:"+el.day.condition.icon} alt="Weather Icon" />
           </div>
           )
         }
